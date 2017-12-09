96a59222164d11d5b1fbefe1b321ae0509f66fbf
Neil Han <neil_han@intuit.com>
2017-12-08 16:10:21 -0800

    char8, import hiding

--- Projects/my-project/src/Main.hs
+++ Projects/my-project/src/Main.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
+-- {-# LANGUAGE NoImplicitPrelude #-}
 
 module Main where
 
-import System.IO (readFile)
+import Prelude -- hiding () -- (Int, foldl, fromInteger, (+), (.), ($), IO, show, getLine, readLn, (++))
+
 import Data.Time (getCurrentTime)
-import Data.ByteString.Lazy.Char8 as Char8
+import Data.ByteString.Lazy.Char8 as Char8 hiding (putStrLn,readFile)
 import Data.Aeson (encode)
 
 import MyFunctions (greet, doPrintMsg)
@@ -17,44 +19,44 @@ main = main3
 -- ask for how many times
 -- print
 main3 = do
-  Prelude.putStrLn "Provide a message:"
+  putStrLn "Provide a message:"
   msg <- getLine
-  Prelude.putStrLn "Provide an integer:"
+  putStrLn "Provide an integer:"
   n <- readLn :: IO Int
-  Prelude.putStrLn $ "Repetting message " ++ show n ++ " times:"
-  -- sequence_ [Prelude.putStrLn msg | i <- [1..n]]
-  replicateM_ n (Prelude.putStrLn msg)
+  putStrLn $ "Repetting message " ++ show n ++ " times:"
+  -- sequence_ [putStrLn msg | i <- [1..n]]
+  replicateM_ n (putStrLn msg)
   -- doPrintMsg msg n
 
 main2 = do
-  let action = Prelude.putStrLn "Hello World"
+  let action = putStrLn "Hello World"
   action
   return ()
 
 main1 = do
-  Prelude.putStrLn (greet "Neil")
-  Prelude.putStrLn (greet "World")
+  putStrLn (greet "Neil")
+  putStrLn (greet "World")
   printNumbers
   -- printConfig
-  Prelude.putStrLn "Time:"
+  putStrLn "Time:"
   printTime
   -- encode [Int]
   printIntList
 
 
 printIntList = do
-  Prelude.putStrLn getEncodedIntList
+  putStrLn getEncodedIntList
 
 getEncodedIntList = Char8.unpack $ encode [1::Int, 2, 3]
 
 
 printNumbers = do
-  Prelude.putStrLn (show (3 + 4 ))
+  putStrLn (show (3 + 4 ))
 
 printConfig = do
-  content <- Prelude.readFile "stack.yaml"
-  Prelude.putStrLn content
+  content <- readFile "stack.yaml"
+  putStrLn content
 
 printTime = do
   time <- getCurrentTime
-  Prelude.putStrLn (show time)
+  putStrLn (show time)
