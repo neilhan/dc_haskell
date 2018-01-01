dcbe5d7ca05233b50dbfdb6809da732e1c6ccb5a
Neil <neil_han@intuit.com>
2018-01-01 13:35:42 -0800

    week2 works

--- Projects/cis194/src/Week2/LogAnalysis.hs
+++ Projects/cis194/src/Week2/LogAnalysis.hs
@@ -6,7 +6,40 @@ import Week2.Log
 import Text.ParserCombinators.ReadP
 import Data.List.Split (splitOn)
 
+-- filter error,
+-- sort; filter E >50; return [string]
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong msgs =
+  fmap (\(LogMessage _ _ str)-> str)
+    [e | e <- (inOrder (buildTree msgs)),
+      case e of
+        (LogMessage (Error level) _ _) -> if level > 50 then True else False
+        _ -> False]
 
+-- sort all messages
+inOrder :: MessageTree -> [LogMessage]
+inOrder tree =
+  case tree of
+    Node l msg r -> (inOrder l) ++ [msg] ++ (inOrder r)
+    Leaf -> []
+
+
+-- build a sorted tree
+buildTree :: [LogMessage] -> MessageTree
+buildTree = foldl (\tree l -> insert l tree) Leaf
+
+-- MessageTree operation
+insert :: LogMessage -> MessageTree -> MessageTree
+insert msg tree =
+  case (msg, tree) of
+    (Unknown _, _ )               -> tree
+    (m@(LogMessage _ ts _), Leaf) -> Node Leaf m Leaf
+    (m@(LogMessage _ ts _), (Node l nm@(LogMessage _ nmts _) r)) ->
+        case ts < nmts of
+          True  -> Node (insert m l) nm r
+          False -> Node l nm (insert m r)
+
+-- parse message
 parse :: String -> [LogMessage]
 parse lines = fmap parseMessage (splitOn "\n" lines )
 
@@ -15,7 +48,7 @@ parseMessage :: String -> LogMessage
 parseMessage line =
   case (readP_to_S logMessageParser line) of
     (a,_):_ -> a
-    _ -> LogMessage Info 0 ("---failed---: " ++ line)
+    _ -> Unknown line
 
 
 logMessageParser :: ReadP LogMessage
