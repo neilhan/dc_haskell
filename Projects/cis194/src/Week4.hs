535c5caaac83f43a697f313efbf8f6945a5ed682
Neil <neil_han@intuit.com>
2018-01-09 22:49:53 -0800

    started cis194 week4

---
+++ Projects/cis194/src/Week4.hs
@@ -0,0 +1,25 @@
+module Week4 where
+
+fun1 :: [Integer] -> Integer
+fun1 [] = 1
+fun1 (x:xs)
+  | even x = (x - 2) * fun1 xs
+  | otherwise = fun1 xs
+
+fun1' :: [Integer] -> Integer
+fun1' = product . map (subtract 2) . filter even
+
+
+fun2 :: Integer -> Integer
+fun2 1 = 0
+fun2 n
+  | even n = n + fun2 (n `div` 2)
+  | otherwise = fun2 (3 * n + 1)
+
+
+fun2' :: Integer -> Integer
+fun2' =
+  sum
+  . filter even
+  . takeWhile (/=1)
+  . iterate (\n -> if even n then div n 2 else 3 * n + 1)
