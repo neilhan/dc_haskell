1877482a8467c0acf1ca87936903858ac8104046
Neil Han <neil_han@intuit.com>
2018-02-25 20:54:09 -0800

    wip - week11

--- Projects/cis194/README.md
+++ Projects/cis194/README.md
@@ -1,1 +1,5 @@
 # cis194
+My workstation crashed once during the week 10, I lost some homework. If you are looking for
+solutions, you can checkout https://github.com/bschwb/cis194-solutions
+
+I learned much from bschwb.
--- Projects/cis194/src/Week11/SExpr.hs
+++ Projects/cis194/src/Week11/SExpr.hs
@@ -4,28 +4,73 @@
 
 module Week11.SExpr where
 
-import AParser
+import Week11.AParser
 import Control.Applicative
+import Data.Char
 
 ------------------------------------------------------------
 --  1. Parsing repetitions
 ------------------------------------------------------------
 
+-- solution learded from https://github.com/bschwb/cis194-solutions/blob/master/11-applicative-functors-part2/SExpr.hs
 zeroOrMore :: Parser a -> Parser [a]
-zeroOrMore p = undefined
+zeroOrMore p = oneOrMore p <|> pure []
 
 oneOrMore :: Parser a -> Parser [a]
-oneOrMore p = undefined
+oneOrMore p = (:) <$> p <*> zeroOrMore p
+
+{-
+-- the solution I had first.
+zeroOrMore_ :: Parser a -> Parser [a]
+zeroOrMore_ p =
+  Parser (f [])
+  where
+    f ary str =
+      case runParser pa str of
+        Nothing -> Just (ary, str)
+        Just (a, str_) -> f (ary++[a]) str_
+
+oneOrMore_ :: Parser a -> Parser [a]
+oneOrMore_ p =
+  Parser (f [])
+  where
+    f ary str =
+      case runParser pa str of
+        Nothing -> case ary of
+                    [] -> Nothing
+                    otherwise -> Just (ary, str)
+        Just (a, str_) -> f (ary++[a]) str_
+
+*AParser> runParser (zeroOrMore (satisfy isUpper)) "ABCdEfgH"
+Just ("ABC","dEfgH")
+*AParser> runParser (oneOrMore (satisfy isUpper)) "ABCdEfgH"
+Just ("ABC","dEfgH")
+*AParser> runParser (zeroOrMore (satisfy isUpper)) "abcdeFGh"
+Just ("","abcdeFGh")
+*AParser> runParser (oneOrMore (satisfy isUpper)) "abcdeFGh"
+Nothing
+-}
 
 ------------------------------------------------------------
 --  2. Utilities
 ------------------------------------------------------------
 
 spaces :: Parser String
-spaces = undefined
+spaces = zeroOrMore $ char ' '
 
 ident :: Parser String
-ident = undefined
+ident = liftA2 (:) (satisfy isAlpha) (zeroOrMore $ satisfy isAlphaNum)
+
+{-
+*AParser> runParser ident "foobar baz"
+Just ("foobar"," baz")
+*AParser> runParser ident "foo33fA"
+Just ("foo33fA","")
+*AParser> runParser ident "2bad"
+Nothing
+*AParser> runParser ident ""
+Nothing
+-}
 
 ------------------------------------------------------------
 --  3. Parsing S-expressions
@@ -44,3 +89,28 @@ data Atom = N Integer | I Ident
 data SExpr = A Atom
            | Comb [SExpr]
   deriving Show
+
+parseAtomIdent :: Parser Atom
+parseAtomIdent = I <$> ( spaces *> ident <* spaces )
+
+parseAtomInt :: Parser Atom
+parseAtomInt = N <$> ( spaces *> posInt <* spaces )
+
+parseAtom :: Parser Atom
+parseAtom = parseAtomInt <|> parseAtomIdent
+
+parseSExprAtom :: Parser SExpr
+parseSExprAtom =
+    A <$> ( parseAtom
+            <|> (spaces *> ((char '(') *> parseAtom <* (char ')')) <* spaces) )
+
+parseSExprComb :: Parser SExpr
+parseSExprComb =
+    Comb <$>
+        (spaces *>
+            ((char '(') *> oneOrMore parseSExpr <* (char ')'))
+        <* spaces)
+
+parseSExpr :: Parser SExpr
+parseSExpr = (parseSExprAtom <|> parseSExprComb)
+
